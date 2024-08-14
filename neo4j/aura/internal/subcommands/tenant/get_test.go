e560daac1de9c16955fd4eea9ec27dea32dbe5de
Darrell Warde <darrell.warde@gmail.com>
2024-08-14 11:29:43 +0100

    tenant command tests to use test helper

--- neo4j/aura/internal/subcommands/tenant/get_test.go
+++ neo4j/aura/internal/subcommands/tenant/get_test.go
@@ -1,93 +1,38 @@
 package tenant_test
 
 import (
-	"bytes"
-	"context"
 	"fmt"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/neo4j/cli/common/clicfg"
-	"github.com/neo4j/cli/common/clictx"
-	"github.com/neo4j/cli/neo4j/aura"
-	"github.com/neo4j/cli/test/utils/testfs"
-	"github.com/stretchr/testify/assert"
+	"github.com/neo4j/cli/neo4j/aura/internal/test/testutils"
 )
 
 func TestGetTenant(t *testing.T) {
-	assert := assert.New(t)
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
 
-	var tenantId = "6981ace7-efe8-4f5c-b7c5-267b5162ce91"
+	tenantId := "6981ace7-efe8-4f5c-b7c5-267b5162ce91"
 
-	mux := http.NewServeMux()
-
-	var authCounter = 0
-	mux.HandleFunc("/oauth/token", func(res http.ResponseWriter, req *http.Request) {
-		authCounter++
-		res.WriteHeader(200)
-		res.Write([]byte(`{"access_token":"12345678","expires_in":3600,"token_type":"bearer"}`))
-	})
-
-	var getCounter = 0
-	mux.HandleFunc(fmt.Sprintf("/v1/tenants/%s", tenantId), func(res http.ResponseWriter, req *http.Request) {
-		getCounter++
-
-		assert.Equal(http.MethodGet, req.Method)
-		assert.Equal(fmt.Sprintf("/v1/tenants/%s", tenantId), req.URL.Path)
-
-		res.WriteHeader(200)
-		res.Write([]byte(`{
+	mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v1/tenants/%s", tenantId), http.StatusOK, `{
 			"data": {
 				"id": "6981ace7-efe8-4f5c-b7c5-267b5162ce91",
 				"name": "Production",
 				"instance_configurations": []
 			}
-		}`))
-	})
+		}`)
 
-	server := httptest.NewServer(mux)
-	defer server.Close()
+	helper.ExecuteCommand(fmt.Sprintf("tenant get %s", tenantId))
 
-	cmd := aura.NewCmd()
-	b := bytes.NewBufferString("")
-	cmd.SetOut(b)
-	cmd.SetArgs([]string{"tenant", "get", "--auth-url", fmt.Sprintf("%s/oauth/token", server.URL), "--base-url", fmt.Sprintf("%s/v1", server.URL), tenantId})
+	mockHandler.AssertCalledTimes(1)
+	mockHandler.AssertCalledWithMethod(http.MethodGet)
 
-	fs, err := testfs.GetTestFs(`{
-		"aura": {
-			"credentials": [{
-				"name": "test-cred",
-				"access-token": "dsa",
-				"token-expiry": 123
-			}],
-			"default-credential": "test-cred"
+	helper.AssertOutJson(`{
+		"data": {
+			"id": "6981ace7-efe8-4f5c-b7c5-267b5162ce91",
+			"name": "Production",
+			"instance_configurations": []
 		}
-	}`)
-	assert.Nil(err)
-
-	cfg, err := clicfg.NewConfig(fs)
-	assert.Nil(err)
-
-	ctx, err := clictx.NewContext(context.Background(), cfg, "test")
-	assert.Nil(err)
-
-	err = cmd.ExecuteContext(ctx)
-	assert.Nil(err)
-
-	out, err := io.ReadAll(b)
-	assert.Nil(err)
-
-	assert.Equal(1, authCounter)
-	assert.Equal(1, getCounter)
-
-	assert.Equal(`{
-	"data": {
-		"id": "6981ace7-efe8-4f5c-b7c5-267b5162ce91",
-		"name": "Production",
-		"instance_configurations": []
 	}
-}
-`, string(out))
+	`)
 }
