b43326d7c0735f7df7f8f61b4ca9a77cfe6d8e66
Alexander Bouriakov <alexander.bouriakov@neo4j.com>
2024-10-01 15:34:56 +0200

    Add instance CMI endpoint URL (#44)
    * instance get: add CMI endpoint URL
    
    * remove stdout
    
    * changelog
    
    * CMI endpoint URL should only be shown if relevant
    
    * remove instance type logic from Aura CLI, rely on logic in Aura API
    
    * improve code
    
    * add a check
    
    * Update and rename Changed-20240926-153446.yaml to Added-20240926-153446.yaml
    
    ---------
    
    Co-authored-by: Darrell Warde <8117355+darrellwarde@users.noreply.github.com>

--- neo4j/aura/internal/subcommands/instance/get.go
+++ neo4j/aura/internal/subcommands/instance/get.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/spf13/cobra"
+
 	"github.com/neo4j/cli/common/clicfg"
 	"github.com/neo4j/cli/neo4j/aura/internal/api"
 	"github.com/neo4j/cli/neo4j/aura/internal/output"
-	"github.com/spf13/cobra"
 )
 
 func NewGetCmd(cfg *clicfg.Config) *cobra.Command {
@@ -17,7 +18,8 @@ func NewGetCmd(cfg *clicfg.Config) *cobra.Command {
 		Long:  "This endpoint returns details about a specific Aura Instance.",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			path := fmt.Sprintf("/instances/%s", args[0])
+			instanceId := args[0]
+			path := fmt.Sprintf("/instances/%s", instanceId)
 
 			cmd.SilenceUsage = true
 			resBody, statusCode, err := api.MakeRequest(cfg, http.MethodGet, path, nil)
@@ -26,14 +28,41 @@ func NewGetCmd(cfg *clicfg.Config) *cobra.Command {
 			}
 
 			if statusCode == http.StatusOK {
-				err = output.PrintBody(cmd, cfg, resBody, []string{"id", "name", "tenant_id", "status", "connection_url", "cloud_provider", "region", "type", "memory", "storage", "customer_managed_key_id"})
+				fields, err := getFields(resBody)
 				if err != nil {
 					return err
 				}
-
+				err = output.PrintBody(cmd, cfg, resBody, fields)
+				if err != nil {
+					return err
+				}
 			}
 
 			return nil
 		},
 	}
+}
+
+func getFields(resBody []byte) ([]string, error) {
+	instances, err := api.ParseBody(resBody)
+	if err != nil {
+		return nil, err
+	}
+	if len(instances) != 1 {
+		return nil, fmt.Errorf("expected 1 instance, got %d", len(instances))
+	}
+	fields := []string{"id", "name", "tenant_id", "status", "connection_url", "cloud_provider", "region", "type", "memory", "storage", "customer_managed_key_id"}
+	if HasCmiEndpoint(instances[0]) {
+		fields = append(fields, "metrics_integration_url")
+	}
+	return fields, nil
+}
+
+func HasCmiEndpoint(instance map[string]any) bool {
+	cmiEndpointUrl := instance["metrics_integration_url"]
+	switch cmiEndpointUrl := cmiEndpointUrl.(type) {
+	case string:
+		return len(cmiEndpointUrl) > 0
+	}
+	return false
 }
