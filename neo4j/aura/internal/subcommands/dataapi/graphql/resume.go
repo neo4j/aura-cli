e8e67c8e6402d5980a4fefd1af4bdb7e60d2b70c
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-10-22 11:24:17 +0200

    feat: resume cmd

---
+++ neo4j/aura/internal/subcommands/dataapi/graphql/resume.go
@@ -0,0 +1,48 @@
+package graphql
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j/aura/internal/api"
+	"github.com/neo4j/cli/neo4j/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewResumeCmd(cfg *clicfg.Config) *cobra.Command {
+	var instanceId string
+
+	cmd := &cobra.Command{
+		Use:   "resume <id>",
+		Short: "Resume a GraphQL Data API",
+		Long: `This endpoint starts the resuming process of an existing Aura GraphQL Data API.
+
+Resuming a GraphQL Data API is an asynchronous operation. You can poll the current status of this operation by periodically getting the GraphQL Data API details for the GraphQL Data API ID using the GET /data-apis/graphql/{data-apiId} endpoint. Once the status transitions from "resuming" to "ready" you may begin to use your GraphQL Data API.	`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			path := fmt.Sprintf("/instances/%s/data-apis/graphql/%s/resume", instanceId, args[0])
+
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				Method: http.MethodPost,
+			})
+			if err != nil {
+				return err
+			}
+
+			if statusCode == http.StatusAccepted {
+				err = output.PrintBody(cmd, cfg, resBody, []string{"id", "name", "status", "url"})
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&instanceId, "instance-id", "", "The ID of the instance to resume the Data API for")
+	cmd.MarkFlagRequired("instance-id")
+
+	return cmd
+}
