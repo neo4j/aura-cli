12b84301b2adbd039daf0c0857ea30b51d71db5a
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-10-22 11:24:02 +0200

    feat: pause cmd

---
+++ neo4j/aura/internal/subcommands/dataapi/graphql/pause.go
@@ -0,0 +1,48 @@
+package graphql
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j/aura/internal/api"
+	"github.com/neo4j/cli/neo4j/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewPauseCmd(cfg *clicfg.Config) *cobra.Command {
+	var instanceId string
+
+	cmd := &cobra.Command{
+		Use:   "pause <id>",
+		Short: "Pause a GraphQL Data API",
+		Long: `This endpoint starts the pausing process of an existing Aura GraphQL Data API.
+
+Pausing a GraphQL Data API is an asynchronous operation. You can poll the current status of this operation by periodically getting the GraphQL Data API details for the GraphQL Data API ID using the GET /data-apis/graphql/{data-apiId} endpoint. The GraphQL Data API will only be paused once the status transitions from "pausing" to "paused".`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cmd.SilenceUsage = true
+			path := fmt.Sprintf("/instances/%s/data-apis/graphql/%s/pause", instanceId, args[0])
+
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				Method: http.MethodPost,
+			})
+			if err != nil {
+				return err
+			}
+
+			if statusCode == http.StatusAccepted {
+				err = output.PrintBody(cmd, cfg, resBody, []string{"id", "name", "status", "url"})
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&instanceId, "instance-id", "", "The ID of the instance to pause the Data API for")
+	cmd.MarkFlagRequired("instance-id")
+
+	return cmd
+}
