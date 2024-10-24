a87882a1882b56ea29052e65ef43758e65a004f0
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-10-24 16:15:16 +0200

    feat: update copy

--- neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/auth_provider.go
+++ neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/auth_provider.go
@@ -8,7 +8,7 @@ import (
 func NewCmd(cfg *clicfg.Config) *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "auth-provider",
-		Short: "Allows you to programmatically manage auth providers for a specific GraphQL Data API",
+		Short: "Allows you to programmatically manage authentication providers for a specific GraphQL Data API",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 			if err := cfg.Aura.BindBaseUrl(cmd.Flags().Lookup("base-url")); err != nil {
 				return err
