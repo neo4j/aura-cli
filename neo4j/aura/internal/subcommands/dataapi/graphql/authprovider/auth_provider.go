8b33e9b31c9c0d7a544d4ef2086952c4effa6e99
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-10-29 14:50:17 +0100

    feat: use auth-providers for endpoint

--- neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/auth_provider.go
+++ neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/auth_provider.go
@@ -9,27 +9,10 @@ func NewCmd(cfg *clicfg.Config) *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "auth-provider",
 		Short: "Allows you to programmatically manage authentication providers for a specific GraphQL Data API",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			if err := cfg.Aura.BindBaseUrl(cmd.Flags().Lookup("base-url")); err != nil {
-				return err
-			}
-			if err := cfg.Aura.BindAuthUrl(cmd.Flags().Lookup("auth-url")); err != nil {
-				return err
-			}
-			if err := cfg.Aura.BindOutput(cmd.Flags().Lookup("output")); err != nil {
-				return err
-			}
-
-			return nil
-		},
 	}
 
 	cmd.AddCommand(NewListCmd(cfg))
 	cmd.AddCommand(NewGetCmd(cfg))
-
-	cmd.PersistentFlags().String("auth-url", "", "")
-	cmd.PersistentFlags().String("base-url", "", "")
-	cmd.PersistentFlags().String("output", "", "")
 
 	return cmd
 }
