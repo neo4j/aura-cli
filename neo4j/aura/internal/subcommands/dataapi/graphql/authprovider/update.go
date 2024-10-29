176b534c71f8c3441844332adf71b0994b49eb70
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-10-29 08:49:21 +0100

    feat: update base cmd

---
+++ neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/update.go
@@ -0,0 +1,107 @@
+package authprovider
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j/aura/internal/api"
+	"github.com/neo4j/cli/neo4j/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewUpdateCmd(cfg *clicfg.Config) *cobra.Command {
+	const (
+		instanceIdFlag = "instance-id"
+		dataApiIdFlag  = "data-api-id"
+		nameFlag       = "name"
+		enabledFlag    = "enabled"
+		urlFlag        = "url"
+		awaitFlag      = "await"
+	)
+
+	var (
+		instanceId string
+		dataApiId  string
+		name       string
+		enabled    string
+		url        string
+		await      bool
+	)
+
+	cmd := &cobra.Command{
+		Use:   "update <id>",
+		Short: "Edit a GraphQL Data API authentication provider",
+		Long: `This endpoint edits a specific GraphQL Data API authentication provider.
+		
+Updating a GraphQL Data API authentication provider is an asynchronous operation. Use the --await flag to wait for the GraphQL Data API to be ready again. Once the status transitions from "updating" to "ready" you may continue to use your GraphQL Data API.`,
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			body := map[string]any{}
+
+			if name != "" {
+				body["name"] = name
+			}
+
+			if enabled != "" {
+				isEnabled, err := strconv.ParseBool(enabled)
+				if err != nil {
+					return fmt.Errorf("invalid value for boolean enabled, err: %s", err.Error())
+				}
+				body["enabled"] = isEnabled
+			}
+
+			if url != "" {
+				body["url"] = url
+			}
+
+			cmd.SilenceUsage = true
+			path := fmt.Sprintf("/instances/%s/data-apis/graphql/%s/auth-provider/%s", instanceId, dataApiId, args[0])
+
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				Method:   http.MethodPatch,
+				PostBody: body,
+			})
+			if err != nil {
+				return err
+			}
+
+			// NOTE: GraphQL Data API update should not return OK (200), it always returns 202, checking both just in case
+			if statusCode == http.StatusAccepted || statusCode == http.StatusOK {
+				output.PrintBody(cmd, cfg, resBody, []string{"id", "name", "type", "enabled", "url"})
+
+				//
+				// TODO: implement await
+				//
+
+				// if await {
+				// 	cmd.Println("Waiting for GraphQL Data API to be updated...")
+				// 	pollResponse, err := api.PollGraphQLDataApi(cfg, instanceId, args[0], api.GraphQLDataApiStatusUpdating)
+				// 	if err != nil {
+				// 		return err
+				// 	}
+
+				// 	cmd.Println("GraphQL Data API Status:", pollResponse.Data.Status)
+				// }
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&instanceId, instanceIdFlag, "", "The ID of the instance the GraphQL Data API is connected to")
+	cmd.MarkFlagRequired(instanceIdFlag)
+
+	cmd.Flags().StringVar(&dataApiId, dataApiIdFlag, "", "The ID of the GraphQL Data API to update the authentication providers for")
+	cmd.MarkFlagRequired(dataApiIdFlag)
+
+	cmd.Flags().StringVar(&name, nameFlag, "", "The name of the authentication provider")
+
+	cmd.Flags().StringVar(&enabled, enabledFlag, "", "Wether or not the authentication provider is enabled")
+
+	cmd.Flags().StringVar(&url, urlFlag, "", "The url for the JWKS endpoint, NOTE: only applicable for authentication provider type 'jwks'")
+
+	cmd.Flags().BoolVar(&await, awaitFlag, false, "Waits until updated GraphQL Data API is ready again.")
+
+	return cmd
+}
---
+++ neo4j/aura/internal/subcommands/dataapi/graphql/authprovider/update_test.go
@@ -0,0 +1,123 @@
+package authprovider_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/neo4j/cli/neo4j/aura/internal/test/testutils"
+)
+
+func TestUpdateAuthProviderFlagsValidation(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	helper.SetConfigValue("aura.beta-enabled", true)
+
+	instanceId := "2f49c2b3"
+	dataApiId := "afdb4e9d"
+	authProviderId := "929d4180-4d28-48fc-8e6e-6018613f0b52"
+
+	tests := map[string]struct {
+		executedCommand string
+		expectedError   string
+	}{
+		"enabled flag has invalid value": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --enabled fg %s", instanceId, dataApiId, authProviderId),
+			expectedError:   "Error: invalid value for boolean enabled, err: strconv.ParseBool: parsing \"fg\": invalid syntax",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			helper.ExecuteCommand(tt.executedCommand)
+			helper.AssertErr(tt.expectedError)
+		})
+	}
+}
+
+func TestUpdateAuthProviderWithResponse(t *testing.T) {
+	instanceId := "2f49c2b3"
+	dataApiId := "75a234b5"
+	authProviderId := "929d4180-4d28-48fc-8e6e-6018613f0b52"
+	name := "api-key-32"
+	enabled := "false"
+	enabledShortHand := "t"
+	url := "https://test.com/.well-known/jwks.json"
+
+	mockResponse := `{
+		"data": {
+			"id": "929d4180-4d28-48fc-8e6e-6018613f0b52",
+			"name": "api-key-32",
+			"enabled": "false",
+			"type": "jwks",
+			"url": "https://test.com/.well-known/jwks.json"
+		}
+	}`
+	expectedResponse := `{
+		"data": {
+			"enabled": "false",
+			"id": "929d4180-4d28-48fc-8e6e-6018613f0b52",
+			"name": "api-key-32",
+			"type": "jwks",
+			"url": "https://test.com/.well-known/jwks.json"
+        }
+	}`
+
+	tests := map[string]struct {
+		mockResponse        string
+		executeCommand      string
+		expectedRequestBody string
+		expectedResponse    string
+	}{
+		"update the name": {
+			mockResponse:        mockResponse,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --name %s %s", instanceId, dataApiId, name, authProviderId),
+			expectedRequestBody: `{"name":"api-key-32"}`,
+			expectedResponse:    expectedResponse,
+		},
+		"update enabled value": {
+			mockResponse:        mockResponse,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --enabled %s %s", instanceId, dataApiId, enabled, authProviderId),
+			expectedRequestBody: `{"enabled":false}`,
+			expectedResponse:    expectedResponse,
+		},
+		"update enabled value short hand": {
+			mockResponse:        mockResponse,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --enabled %s %s", instanceId, dataApiId, enabledShortHand, authProviderId),
+			expectedRequestBody: `{"enabled":true}`,
+			expectedResponse:    expectedResponse,
+		},
+		"update url": {
+			mockResponse:        mockResponse,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --url %s %s", instanceId, dataApiId, url, authProviderId),
+			expectedRequestBody: `{"url":"https://test.com/.well-known/jwks.json"}`,
+			expectedResponse:    expectedResponse,
+		},
+		"update all possible values in one request": {
+			mockResponse:        mockResponse,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider update --output json --instance-id %s --data-api-id %s --name %s --enabled %s --url %s %s", instanceId, dataApiId, name, enabled, url, authProviderId),
+			expectedRequestBody: `{"enabled":false,"name":"api-key-32","url":"https://test.com/.well-known/jwks.json"}`,
+			expectedResponse:    expectedResponse,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			helper := testutils.NewAuraTestHelper(t)
+			defer helper.Close()
+
+			helper.SetConfigValue("aura.beta-enabled", true)
+
+			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v1/instances/%s/data-apis/graphql/%s/auth-provider/%s", instanceId, dataApiId, authProviderId), http.StatusAccepted, tt.mockResponse)
+
+			helper.ExecuteCommand(tt.executeCommand)
+
+			mockHandler.AssertCalledTimes(1)
+			mockHandler.AssertCalledWithMethod(http.MethodPatch)
+			mockHandler.AssertCalledWithBody(tt.expectedRequestBody)
+
+			helper.AssertOutJson(tt.expectedResponse)
+		})
+	}
+}
