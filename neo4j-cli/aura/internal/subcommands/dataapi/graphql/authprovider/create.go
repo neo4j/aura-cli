a4183a8e95db1ee62014691a56c71d4aa9fe20b6
Thomas Wiss <thomas.wiss@neotechnology.com>
2024-11-07 16:54:34 +0100

    data-api: Auth provider create command (#92)
    * feat: list cmd base
    
    * fix: delete file
    
    * feat: update copy
    
    * feat: add get cmd
    
    * feat: use auth-providers for endpoint
    
    * feat: wip
    
    * feat: more tests
    
    * feat: files move
    
    * feat: copy update
    
    * feat: update err text
    
    * feat: url flag dg
    
    * feat: changie
    
    * feat: updates after merge
    
    * Update neo4j-cli/aura/internal/subcommands/dataapi/graphql/authprovider/create.go
    
    Co-authored-by: Darrell Warde <8117355+darrellwarde@users.noreply.github.com>
    
    * feat: enabled as bool flag
    
    * feat: url flag can not be set for api-key
    
    ---------
    
    Co-authored-by: Darrell Warde <8117355+darrellwarde@users.noreply.github.com>

---
+++ neo4j-cli/aura/internal/subcommands/dataapi/graphql/authprovider/create.go
@@ -0,0 +1,132 @@
+package authprovider
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/api"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewCreateCmd(cfg *clicfg.Config) *cobra.Command {
+	const (
+		instanceIdFlag = "instance-id"
+		dataApiIdFlag  = "data-api-id"
+		typeFlag       = "type"
+		nameFlag       = "name"
+		enabledFlag    = "enabled"
+		urlFlag        = "url"
+		awaitFlag      = "await"
+
+		enabledDefault = false
+	)
+
+	var (
+		instanceId string
+		dataApiId  string
+		_type      string
+		name       string
+		enabled    bool
+		url        string
+		await      bool
+	)
+
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Creates a new GraphQL Data API authentication provider",
+		Long: `This command creates a new GraphQL Data API authentication provider.
+
+Creating a GraphQL Data API authentication provider is an asynchronous operation. Use the --await flag to wait for the GraphQL Data API to be ready. Once the status transitions from "creating" to "ready" you may begin to use your GraphQL Data API.
+
+If you create an 'api-key' Authentication provider, an API key will be created. It is important to store the API key as it is not currently possible to get it or update it.
+
+If you lose your API key, you will need to create a new Authentication provider. This will not result in any loss of data.`,
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			typeValue, _ := cmd.Flags().GetString(typeFlag)
+			if typeValue == api.GraphQLDataApiAuthProviderTypeJwks {
+				cmd.MarkFlagRequired(urlFlag)
+			}
+
+			if typeValue == api.GraphQLDataApiAuthProviderTypeApiKey && url != "" {
+				return fmt.Errorf("url flag can not be set for authentication provider type '%s'", api.GraphQLDataApiAuthProviderTypeApiKey)
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if _type != api.GraphQLDataApiAuthProviderTypeJwks && _type != api.GraphQLDataApiAuthProviderTypeApiKey {
+				msg := strings.ToLower(fmt.Sprintf("invalid authentication provider type, got '%s', expected '%s' or '%s'", _type, api.GraphQLDataApiAuthProviderTypeJwks, api.GraphQLDataApiAuthProviderTypeApiKey))
+				return errors.New(msg)
+			}
+
+			body := map[string]any{
+				"type":    _type,
+				"name":    name,
+				"enabled": enabled,
+			}
+
+			if url != "" {
+				body["url"] = url
+			}
+
+			cmd.SilenceUsage = true
+			path := fmt.Sprintf("/instances/%s/data-apis/graphql/%s/auth-providers", instanceId, dataApiId)
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				PostBody: body,
+				Method:   http.MethodPost,
+			})
+			if err != nil {
+				return err
+			}
+
+			// NOTE: Auth provider create should not return OK (200), it always returns 202, checking both just in case
+			if statusCode == http.StatusAccepted || statusCode == http.StatusOK {
+
+				if _type == api.GraphQLDataApiAuthProviderTypeApiKey {
+					cmd.Println("###############################")
+					cmd.Println("# It is important to store the created API key! If you lose your API key, you will need to create a new Authentication provider. This will not result in any loss of data.")
+					cmd.Println("###############################")
+				}
+
+				output.PrintBody(cmd, cfg, resBody, []string{"id", "name", "type", "enabled", "key", "url"})
+
+				if await {
+					cmd.Println("Waiting for GraphQL Data API to be ready...")
+					pollResponse, err := api.PollGraphQLDataApi(cfg, instanceId, dataApiId, api.GraphQLDataApiStatusCreating)
+					if err != nil {
+						return err
+					}
+
+					cmd.Println("GraphQL Data API Status:", pollResponse.Data.Status)
+				}
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&instanceId, instanceIdFlag, "", "The ID of the instance to create the GraphQL Data API for")
+	cmd.MarkFlagRequired(instanceIdFlag)
+
+	cmd.Flags().StringVar(&dataApiId, dataApiIdFlag, "", "The ID of the GraphQL Data API to create the authentication provider for")
+	cmd.MarkFlagRequired(dataApiIdFlag)
+
+	msgTypeFlag := fmt.Sprintf("The type of the Authentication provider, one of '%s' or '%s'", api.GraphQLDataApiAuthProviderTypeApiKey, api.GraphQLDataApiAuthProviderTypeJwks)
+	cmd.Flags().StringVar(&_type, typeFlag, "", msgTypeFlag)
+	cmd.MarkFlagRequired(typeFlag)
+
+	cmd.Flags().StringVar(&name, nameFlag, "", "The name of the Authentication provider")
+	cmd.MarkFlagRequired(nameFlag)
+
+	cmd.Flags().BoolVar(&enabled, enabledFlag, enabledDefault, "Whether or not the Authentication provider is enabled")
+
+	msgUrlFlag := fmt.Sprintf("The JWKS URL that you want the bearer tokens in incoming GraphQL requests to be validated against. NOTE: only applicable for Authentication provider type '%s'", api.GraphQLDataApiAuthProviderTypeJwks)
+	cmd.Flags().StringVar(&url, urlFlag, "", msgUrlFlag)
+
+	cmd.Flags().BoolVar(&await, awaitFlag, false, "Waits until created GraphQL Data API is ready.")
+
+	return cmd
+}
---
+++ neo4j-cli/aura/internal/subcommands/dataapi/graphql/authprovider/create_test.go
@@ -0,0 +1,185 @@
+package authprovider_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/test/testutils"
+)
+
+func TestCreateAuthProviderFlagsValidation(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	helper.SetConfigValue("aura.beta-enabled", true)
+
+	instanceId := "2f49c2b3"
+	dataApiId := "23ea345a"
+	name := "my-key-1"
+
+	tests := map[string]struct {
+		executedCommand string
+		expectedError   string
+	}{
+		"missing all create flags": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s", instanceId, dataApiId),
+			expectedError:   "Error: required flag(s) \"name\", \"type\" not set",
+		},
+		"missing name flag": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --type api-key", instanceId, dataApiId),
+			expectedError:   "Error: required flag(s) \"name\" not set",
+		},
+		"missing type flag": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --enabled", instanceId, dataApiId, name),
+			expectedError:   "Error: required flag(s) \"type\" not set",
+		},
+		"non-existing type flag": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --type bla --enabled", instanceId, dataApiId, name),
+			expectedError:   "Error: invalid authentication provider type, got 'bla', expected 'jwks' or 'api-key'",
+		},
+		"missing url flag for jwks": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --type jwks --enabled", instanceId, dataApiId, name),
+			expectedError:   "Error: required flag(s) \"url\" not set",
+		},
+		"can not set url flag for api-key": {
+			executedCommand: fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --type api-key --url http://test.com/abc", instanceId, dataApiId, name),
+			expectedError:   "Error: url flag can not be set for authentication provider type 'api-key'",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			helper.ExecuteCommand(tt.executedCommand)
+			helper.AssertErr(tt.expectedError)
+		})
+	}
+}
+
+func TestCreateAuthProviderWithResponse(t *testing.T) {
+	instanceId := "2f49c2b3"
+	dataApiId := "23ea345a"
+	nameApiKey := "my-key-2"
+	nameJwks := "my-jwks-2"
+	url := "https://test.com/.well-known/jwks.json"
+
+	mockResponseApiKey := `{
+		"data": {
+			"id": "1ad1b794-e40e-41f7-8e8c-5638130317ed",
+			"name": "my-key-2",
+			"type": "api-key",
+			"enabled": true,
+			"key": "ublHwKxm2ylsc1HlkuL8NAcMfZnEVP1g"
+		}
+	}`
+	mockResponseJwks := `{
+		"data": {
+			"id": "a3435b3-e40e-41f7-8e8c-5638130319aa",
+			"name": "my-jwks-2",
+			"type": "jwks",
+			"enabled": false,
+			"url": "https://test.com/.well-known/jwks.json"
+		}
+	}`
+
+	expectedResponseJsonApiKey := `###############################
+# It is important to store the created API key! If you lose your API key, you will need to create a new Authentication provider. This will not result in any loss of data.
+###############################
+{
+	"data": {
+		"enabled": true,
+		"id": "1ad1b794-e40e-41f7-8e8c-5638130317ed",
+		"key": "ublHwKxm2ylsc1HlkuL8NAcMfZnEVP1g",
+		"name": "my-key-2",
+		"type": "api-key"
+	}
+}`
+	expectedResponseTableApiKey := `###############################
+# It is important to store the created API key! If you lose your API key, you will need to create a new Authentication provider. This will not result in any loss of data.
+###############################
+┌──────────────────────────────────────┬──────────┬─────────┬─────────┬──────────────────────────────────┬─────┐
+│ ID                                   │ NAME     │ TYPE    │ ENABLED │ KEY                              │ URL │
+├──────────────────────────────────────┼──────────┼─────────┼─────────┼──────────────────────────────────┼─────┤
+│ 1ad1b794-e40e-41f7-8e8c-5638130317ed │ my-key-2 │ api-key │ true    │ ublHwKxm2ylsc1HlkuL8NAcMfZnEVP1g │     │
+└──────────────────────────────────────┴──────────┴─────────┴─────────┴──────────────────────────────────┴─────┘
+		`
+	expectedResponseJsonJwks := `{
+	"data": {
+		"enabled": false,
+		"id": "a3435b3-e40e-41f7-8e8c-5638130319aa",
+		"name": "my-jwks-2",
+		"type": "jwks",
+		"url": "https://test.com/.well-known/jwks.json"
+	}
+}`
+	expectedResponseTableJwks := `
+┌─────────────────────────────────────┬───────────┬──────┬─────────┬─────┬────────────────────────────────────────┐
+│ ID                                  │ NAME      │ TYPE │ ENABLED │ KEY │ URL                                    │
+├─────────────────────────────────────┼───────────┼──────┼─────────┼─────┼────────────────────────────────────────┤
+│ a3435b3-e40e-41f7-8e8c-5638130319aa │ my-jwks-2 │ jwks │ false   │     │ https://test.com/.well-known/jwks.json │
+└─────────────────────────────────────┴───────────┴──────┴─────────┴─────┴────────────────────────────────────────┘
+			`
+
+	tests := map[string]struct {
+		mockResponse        string
+		executeCommand      string
+		expectedRequestBody string
+		expectedResponse    string
+	}{
+		"create api-key only with name": {
+			mockResponse:        mockResponseApiKey,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --type api-key", instanceId, dataApiId, nameApiKey),
+			expectedRequestBody: `{"enabled":false,"name":"my-key-2","type":"api-key"}`,
+			expectedResponse:    expectedResponseJsonApiKey,
+		},
+		"create api-key with name and enabled flag": {
+			mockResponse:        mockResponseApiKey,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --type api-key --enabled", instanceId, dataApiId, nameApiKey),
+			expectedRequestBody: `{"enabled":true,"name":"my-key-2","type":"api-key"}`,
+			expectedResponse:    expectedResponseJsonApiKey,
+		},
+		"create api-key with name and enabled flag response as table": {
+			mockResponse:        mockResponseApiKey,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --output table --instance-id %s --data-api-id %s --name %s --type api-key --enabled", instanceId, dataApiId, nameApiKey),
+			expectedRequestBody: `{"enabled":true,"name":"my-key-2","type":"api-key"}`,
+			expectedResponse:    expectedResponseTableApiKey,
+		},
+		"create jwks only with name and url": {
+			mockResponse:        mockResponseJwks,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --url %s --type jwks", instanceId, dataApiId, nameJwks, url),
+			expectedRequestBody: `{"enabled":false,"name":"my-jwks-2","type":"jwks","url":"https://test.com/.well-known/jwks.json"}`,
+			expectedResponse:    expectedResponseJsonJwks,
+		},
+		"create jwks with name and url and enabled flag": {
+			mockResponse:        mockResponseJwks,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --instance-id %s --data-api-id %s --name %s --url %s --type jwks", instanceId, dataApiId, nameJwks, url),
+			expectedRequestBody: `{"enabled":false,"name":"my-jwks-2","type":"jwks","url":"https://test.com/.well-known/jwks.json"}`,
+			expectedResponse:    expectedResponseJsonJwks,
+		},
+		"create jwks with name and url and enabled flag response as table": {
+			mockResponse:        mockResponseJwks,
+			executeCommand:      fmt.Sprintf("data-api graphql auth-provider create --output table --instance-id %s --data-api-id %s --name %s --url %s --type jwks", instanceId, dataApiId, nameJwks, url),
+			expectedRequestBody: `{"enabled":false,"name":"my-jwks-2","type":"jwks","url":"https://test.com/.well-known/jwks.json"}`,
+			expectedResponse:    expectedResponseTableJwks,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			helper := testutils.NewAuraTestHelper(t)
+			defer helper.Close()
+
+			helper.SetConfigValue("aura.beta-enabled", true)
+
+			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v1/instances/%s/data-apis/graphql/%s/auth-providers", instanceId, dataApiId), http.StatusAccepted, tt.mockResponse)
+
+			helper.ExecuteCommand(tt.executeCommand)
+
+			mockHandler.AssertCalledTimes(1)
+			mockHandler.AssertCalledWithMethod(http.MethodPost)
+			mockHandler.AssertCalledWithBody(tt.expectedRequestBody)
+
+			helper.AssertOut(tt.expectedResponse)
+		})
+	}
+}
