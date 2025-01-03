9fa145fa97ba014e4770416faaf5dbc56b0c8bc2
Liam Doodson <liam.doodson@neo4j.com>
2025-01-03 15:46:27 +0000

    Add flag validation UTs

---
+++ neo4j-cli/aura/internal/subcommands/dataapi/graphql/corspolicy/allowedorigin/add_test.go
@@ -0,0 +1,48 @@
+package allowedorigin_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/test/testutils"
+)
+
+func TestAddAllowedOriginFlagsValidation(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	helper.SetConfigValue("aura.beta-enabled", true)
+
+	instanceId := "2f49c2b3"
+	dataApiId := "e157301d"
+	allowedOrigin := "https://test.com"
+
+	tests := map[string]struct {
+		executedCommand string
+		expectedError   string
+	}{
+		"missing all flags": {
+			executedCommand: fmt.Sprintf("data-api graphql cors-policy allowed-origin add %s", allowedOrigin),
+			expectedError:   "Error: required flag(s) \"data-api-id\", \"instance-id\" not set",
+		},
+		"missing origin": {
+			executedCommand: fmt.Sprintf("data-api graphql cors-policy allowed-origin add --data-api-id %s --instance-id %s", dataApiId, instanceId),
+			expectedError:   "Error: accepts 1 arg(s), received 0",
+		},
+		"missing data api id flag": {
+			executedCommand: fmt.Sprintf("data-api graphql cors-policy allowed-origin add %s --instance-id %s", allowedOrigin, instanceId),
+			expectedError:   "Error: required flag(s) \"data-api-id\" not set",
+		},
+		"missing instance id flag": {
+			executedCommand: fmt.Sprintf("data-api graphql cors-policy allowed-origin add %s --data-api-id %s", allowedOrigin, dataApiId),
+			expectedError:   "Error: required flag(s) \"instance-id\" not set",
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			helper.ExecuteCommand(tt.executedCommand)
+			helper.AssertErr(tt.expectedError)
+		})
+	}
+}
