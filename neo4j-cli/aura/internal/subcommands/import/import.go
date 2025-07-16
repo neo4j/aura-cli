672f28605547ab94bc03728d66c82d5cca62f689
Junxiang Chen <chenfrey920417@outlook.com>
2025-07-16 17:48:21 +0100

    chore: add hierarchy

--- neo4j-cli/aura/internal/subcommands/import/import.go
+++ neo4j-cli/aura/internal/subcommands/import/import.go
@@ -2,6 +2,7 @@ package _import
 
 import (
 	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/subcommands/import/jobs"
 	"github.com/spf13/cobra"
 )
 
@@ -10,5 +11,7 @@ func NewCmd(cfg *clicfg.Config) *cobra.Command {
 		Use:   "import",
 		Short: "Allows you to import your data into Aura instances and manage your import jobs",
 	}
+
+	cmd.AddCommand(jobs.NewCmd(cfg))
 	return cmd
 }
