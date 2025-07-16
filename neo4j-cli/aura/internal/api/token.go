be792237dddfc454db3d0f307d89a1db544fd989
Junxiang Chen <chenfrey920417@outlook.com>
2025-07-16 17:14:08 +0100

    feat: PoC on token exchange against dev environment

--- neo4j-cli/aura/internal/api/token.go
+++ neo4j-cli/aura/internal/api/token.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
@@ -36,6 +37,7 @@ func getToken(credential *credentials.AuraCredential, cfg *clicfg.Config) (strin
 		"User-Agent":   {fmt.Sprintf(userAgent, version)},
 	}
 	req.SetBasicAuth(credential.ClientId, credential.ClientSecret)
+	log.Println(fmt.Sprintf("making request to %s, req %+v", url, req))
 
 	client := http.Client{}
 
