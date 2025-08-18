8ff2e4b0656e0816cd864ca665673ff8ea95e9ea
Junxiang Chen <chenfrey920417@outlook.com>
2025-08-18 17:32:28 +0100

    test: add failing test

--- neo4j-cli/aura/internal/subcommands/import/job/create_test.go
+++ neo4j-cli/aura/internal/subcommands/import/job/create_test.go
@@ -1,6 +1,7 @@
 package job_test
 
 import (
+	"fmt"
 	"github.com/neo4j/cli/neo4j-cli/aura/internal/test/testutils"
 	"net/http"
 	"testing"
@@ -35,4 +36,57 @@ func TestCreateImportJob(t *testing.T) {
 			"data": {"id": "87d485b4-73fc-4a7f-bb03-720f4672947e"}
 		}
 	`)
+}
+
+func TestCreateImportJobError(t *testing.T) {
+	testCases := []struct {
+		statusCode    int
+		expectedError string
+		returnBody    string
+	}{
+		{
+			statusCode:    http.StatusBadRequest,
+			expectedError: "Error: [DataSourceId: Import model data source id is required]",
+			returnBody: `{
+				"errors": [
+					{
+					"message": "Import model data source id is required",
+					"reason": "Required",
+					"field": "DataSourceId"
+					}
+				]
+			}`,
+		},
+		{
+			statusCode:    http.StatusMethodNotAllowed,
+			expectedError: "Error: [string]",
+			returnBody: `{
+				"errors": [
+					{
+					"message": "string",
+					"reason": "string",
+					"field": "string"
+					}
+				]
+			}`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(fmt.Sprintf("StatusCode%d", testCase.statusCode), func(t *testing.T) {
+			helper := testutils.NewAuraTestHelper(t)
+			defer helper.Close()
+
+			mockHandler := helper.NewRequestHandlerMock("/v2beta1/projects/f607bebe-0cc0-4166-b60c-b4eed69ee7ee/import/jobs", testCase.statusCode, testCase.returnBody)
+
+			helper.SetConfigValue("aura.beta-enabled", true)
+
+			helper.ExecuteCommand("import job create --project-id=f607bebe-0cc0-4166-b60c-b4eed69ee7ee --import-model-id=e01cdc6d-2f50-4f46-b04b-8ec8fc8de839 --aura-db-id=07e49cf5")
+
+			mockHandler.AssertCalledTimes(1)
+			mockHandler.AssertCalledWithMethod(http.MethodPost)
+
+			helper.AssertErr(testCase.expectedError)
+		})
+	}
 }
