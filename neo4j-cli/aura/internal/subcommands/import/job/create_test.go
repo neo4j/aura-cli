23e7a311137697b25f8126d7f057f1365bc1b30a
Junxiang Chen <chenfrey920417@outlook.com>
2025-08-21 16:22:52 +0100

    test: enhance test case with missing flags

--- neo4j-cli/aura/internal/subcommands/import/job/create_test.go
+++ neo4j-cli/aura/internal/subcommands/import/job/create_test.go
@@ -39,14 +39,21 @@ func TestCreateImportJob(t *testing.T) {
 }
 
 func TestCreateImportJobError(t *testing.T) {
-	testCases := []struct {
-		statusCode    int
-		expectedError string
-		returnBody    string
+	projectId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
+	importModelId := "e01cdc6d-2f50-4f46-b04b-8ec8fc8de839"
+	auraDbId := "07e49cf5"
+	testCases := map[string]struct {
+		executeCommand      string
+		expectedCalledTimes int
+		statusCode          int
+		expectedError       string
+		returnBody          string
 	}{
-		{
-			statusCode:    http.StatusBadRequest,
-			expectedError: "Error: [DataSourceId: Import model data source id is required]",
+		"correct command with error response 1": {
+			executeCommand:      fmt.Sprintf("import job create --project-id=%s --import-model-id=%s --aura-db-id=%s", projectId, importModelId, auraDbId),
+			expectedCalledTimes: 1,
+			statusCode:          http.StatusBadRequest,
+			expectedError:       "Error: [DataSourceId: Import model data source id is required]",
 			returnBody: `{
 				"errors": [
 					{
@@ -57,9 +64,11 @@ func TestCreateImportJobError(t *testing.T) {
 				]
 			}`,
 		},
-		{
-			statusCode:    http.StatusMethodNotAllowed,
-			expectedError: "Error: [string]",
+		"correct command with error response 2": {
+			executeCommand:      fmt.Sprintf("import job create --project-id=%s --import-model-id=%s --aura-db-id=%s", projectId, importModelId, auraDbId),
+			expectedCalledTimes: 1,
+			statusCode:          http.StatusMethodNotAllowed,
+			expectedError:       "Error: [string]",
 			returnBody: `{
 				"errors": [
 					{
@@ -70,21 +79,42 @@ func TestCreateImportJobError(t *testing.T) {
 				]
 			}`,
 		},
+		"incorrect command with missing projectId": {
+			executeCommand:      fmt.Sprintf("import job create --import-model-id=%s --aura-db-id=%s", importModelId, auraDbId),
+			expectedCalledTimes: 0,
+			statusCode:          http.StatusNotFound,
+			expectedError:       "Error: projectId is required",
+			returnBody:          ``,
+		},
+		"incorrect command with missing importModelId": {
+			executeCommand: fmt.Sprintf("import job create --project-id=%s --aura-db-id=%s", projectId, auraDbId),
+			statusCode:     http.StatusNotFound,
+			expectedError:  "Error: importModelId is required",
+			returnBody:     ``,
+		},
+		"incorrect command with missing auraDbId": {
+			executeCommand: fmt.Sprintf("import job create --project-id=%s --import-model-id=%s", projectId, importModelId),
+			statusCode:     http.StatusNotFound,
+			expectedError:  "Error: auraDbId is required",
+			returnBody:     ``,
+		},
 	}
 
-	for _, testCase := range testCases {
-		t.Run(fmt.Sprintf("StatusCode%d", testCase.statusCode), func(t *testing.T) {
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
 			helper := testutils.NewAuraTestHelper(t)
 			defer helper.Close()
 
-			mockHandler := helper.NewRequestHandlerMock("/v2beta1/projects/f607bebe-0cc0-4166-b60c-b4eed69ee7ee/import/jobs", testCase.statusCode, testCase.returnBody)
+			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/projects/%s/import/jobs", projectId), testCase.statusCode, testCase.returnBody)
 
 			helper.SetConfigValue("aura.beta-enabled", true)
 
-			helper.ExecuteCommand("import job create --project-id=f607bebe-0cc0-4166-b60c-b4eed69ee7ee --import-model-id=e01cdc6d-2f50-4f46-b04b-8ec8fc8de839 --aura-db-id=07e49cf5")
+			helper.ExecuteCommand(testCase.executeCommand)
 
-			mockHandler.AssertCalledTimes(1)
-			mockHandler.AssertCalledWithMethod(http.MethodPost)
+			mockHandler.AssertCalledTimes(testCase.expectedCalledTimes)
+			if testCase.expectedCalledTimes > 0 {
+				mockHandler.AssertCalledWithMethod(http.MethodPost)
+			}
 
 			helper.AssertErr(testCase.expectedError)
 		})
