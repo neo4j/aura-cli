54beda78bf4cec7abf7278771c57b55078a8a66b
Junxiang Chen <chenfrey920417@outlook.com>
2025-08-21 16:54:05 +0100

    test: add test case of missing flags and parameters

--- neo4j-cli/aura/internal/subcommands/import/job/get_test.go
+++ neo4j-cli/aura/internal/subcommands/import/job/get_test.go
@@ -601,3 +601,79 @@ func TestGetImportJobById(t *testing.T) {
 		})
 	}
 }
+
+func TestGetImportJobError(t *testing.T) {
+	projectId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
+	jobId := "87d485b4-73fc-4a7f-bb03-720f4672947e"
+	testCases := map[string]struct {
+		executeCommand      string
+		statusCode          int
+		expectedCalledTimes int
+		expectedError       string
+		returnBody          string
+	}{
+		"correct command with error response 1": {
+			executeCommand:      fmt.Sprintf("import job get --project-id=%s %s", projectId, jobId),
+			statusCode:          http.StatusNotFound,
+			expectedCalledTimes: 1,
+			expectedError:       "Error: [The job 87d485b4-73fc-4a7f-bb03-720f4672947e does not exist]",
+			returnBody: `{
+				"errors": [
+					{
+					"message": "The job 87d485b4-73fc-4a7f-bb03-720f4672947e does not exist",
+					"reason": "Requested"
+					}
+				]
+			}`,
+		},
+		"correct command with error response 2": {
+			executeCommand:      fmt.Sprintf("import job get --project-id=%s %s", projectId, jobId),
+			statusCode:          http.StatusMethodNotAllowed,
+			expectedCalledTimes: 1,
+			expectedError:       "Error: [string]",
+			returnBody: `{
+				"errors": [
+					{
+					"message": "string",
+					"reason": "string",
+					"field": "string"
+					}
+				]
+			}`,
+		},
+		"incorrect command with missing project id": {
+			executeCommand:      fmt.Sprintf("import job get %s", jobId),
+			statusCode:          http.StatusBadRequest,
+			expectedCalledTimes: 0,
+			expectedError:       "Error: required flag(s) \"project-id\" not set",
+			returnBody:          ``,
+		},
+		"incorrect command with missing job id": {
+			executeCommand:      fmt.Sprintf("import job get --project-id=%s", projectId),
+			statusCode:          http.StatusBadRequest,
+			expectedCalledTimes: 0,
+			expectedError:       "Error: accepts 1 arg(s), received 0",
+			returnBody:          ``,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			helper := testutils.NewAuraTestHelper(t)
+			defer helper.Close()
+
+			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/projects/%s/import/jobs/%s", projectId, jobId), testCase.statusCode, testCase.returnBody)
+
+			helper.SetConfigValue("aura.beta-enabled", true)
+
+			helper.ExecuteCommand(testCase.executeCommand)
+
+			mockHandler.AssertCalledTimes(testCase.expectedCalledTimes)
+			if testCase.expectedCalledTimes > 0 {
+				mockHandler.AssertCalledWithMethod(http.MethodGet)
+			}
+
+			helper.AssertErr(testCase.expectedError)
+		})
+	}
+}
