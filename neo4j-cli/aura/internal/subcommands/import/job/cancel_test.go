e3ad358232ffa92baf94cb9deff349479b6f1b87
Junxiang Chen <chenfrey920417@outlook.com>
2025-09-12 14:52:10 +0100

    chore: update job cancelling flags

--- neo4j-cli/aura/internal/subcommands/import/job/cancel_test.go
+++ neo4j-cli/aura/internal/subcommands/import/job/cancel_test.go
@@ -8,33 +8,39 @@ import (
 )
 
 func TestCancelImportJob(t *testing.T) {
+	organizationId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
+	projectId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
+	jobId := "87d485b4-73fc-4a7f-bb03-720f4672947e"
+
 	helper := testutils.NewAuraTestHelper(t)
 	defer helper.Close()
 
-	mockHandler := helper.NewRequestHandlerMock("/v2beta1/projects/f607bebe-0cc0-4166-b60c-b4eed69ee7ee/import/jobs/87d485b4-73fc-4a7f-bb03-720f4672947e/cancellation", http.StatusOK, `
+	mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/organizations/%s/projects/%s/import/jobs/%s/cancellation", organizationId, projectId, jobId), http.StatusOK, fmt.Sprintf(`
 		{
-			"data": {"id": "87d485b4-73fc-4a7f-bb03-720f4672947e"}
+			"data": {"id": "%s"}
 		}
-	`)
+	`, jobId))
 
 	helper.SetConfigValue("aura.beta-enabled", true)
 
-	helper.ExecuteCommand("import job cancel --project-id=f607bebe-0cc0-4166-b60c-b4eed69ee7ee 87d485b4-73fc-4a7f-bb03-720f4672947e")
+	helper.ExecuteCommand(fmt.Sprintf("import job cancel --organization-id=%s --project-id=%s %s", organizationId, projectId, jobId))
 
 	mockHandler.AssertCalledTimes(1)
 	mockHandler.AssertCalledWithMethod(http.MethodPost)
 
 	helper.AssertErr("")
-	helper.AssertOutJson(`
+	helper.AssertOutJson(fmt.Sprintf(`
 		{
-			"data": {"id": "87d485b4-73fc-4a7f-bb03-720f4672947e"}
+			"data": {"id": "%s"}
 		}
-	`)
+	`, jobId))
 }
 
 func TestCancelImportJobError(t *testing.T) {
+	organizationId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
 	projectId := "f607bebe-0cc0-4166-b60c-b4eed69ee7ee"
 	jobId := "87d485b4-73fc-4a7f-bb03-720f4672947e"
+
 	testCases := map[string]struct {
 		executeCommand      string
 		statusCode          int
@@ -43,7 +49,7 @@ func TestCancelImportJobError(t *testing.T) {
 		returnBody          string
 	}{
 		"correct command with error response 1": {
-			executeCommand:      fmt.Sprintf("import job cancel --project-id=%s %s", projectId, jobId),
+			executeCommand:      fmt.Sprintf("import job cancel --organization-id=%s --project-id=%s %s", organizationId, projectId, jobId),
 			statusCode:          http.StatusBadRequest,
 			expectedCalledTimes: 1,
 			expectedError:       "Error: [The job 87d485b4-73fc-4a7f-bb03-720f4672947e has requested to cancel]",
@@ -57,7 +63,7 @@ func TestCancelImportJobError(t *testing.T) {
 			}`,
 		},
 		"correct command with error response 2": {
-			executeCommand:      fmt.Sprintf("import job cancel --project-id=%s %s", projectId, jobId),
+			executeCommand:      fmt.Sprintf("import job cancel --organization-id=%s --project-id=%s %s", organizationId, projectId, jobId),
 			statusCode:          http.StatusMethodNotAllowed,
 			expectedCalledTimes: 1,
 			expectedError:       "Error: [string]",
@@ -71,15 +77,22 @@ func TestCancelImportJobError(t *testing.T) {
 				]
 			}`,
 		},
+		"incorrect command with missing organization id": {
+			executeCommand:      fmt.Sprintf("import job cancel --project-id=%s %s", projectId, jobId),
+			statusCode:          http.StatusBadRequest,
+			expectedCalledTimes: 0,
+			expectedError:       "Error: required flag(s) \"organization-id\" not set",
+			returnBody:          ``,
+		},
 		"incorrect command with missing project id": {
-			executeCommand:      fmt.Sprintf("import job cancel %s", jobId),
+			executeCommand:      fmt.Sprintf("import job cancel --organization-id=%s %s", organizationId, jobId),
 			statusCode:          http.StatusBadRequest,
 			expectedCalledTimes: 0,
 			expectedError:       "Error: required flag(s) \"project-id\" not set",
 			returnBody:          ``,
 		},
 		"incorrect command with missing job id": {
-			executeCommand:      fmt.Sprintf("import job cancel --project-id=%s", projectId),
+			executeCommand:      fmt.Sprintf("import job cancel --organization-id=%s --project-id=%s", organizationId, projectId),
 			statusCode:          http.StatusBadRequest,
 			expectedCalledTimes: 0,
 			expectedError:       "Error: accepts 1 arg(s), received 0",
@@ -92,7 +105,7 @@ func TestCancelImportJobError(t *testing.T) {
 			helper := testutils.NewAuraTestHelper(t)
 			defer helper.Close()
 
-			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/projects/%s/import/jobs/%s/cancellation", projectId, jobId), testCase.statusCode, testCase.returnBody)
+			mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/organizations/%s/projects/%s/import/jobs/%s/cancellation", organizationId, projectId, jobId), testCase.statusCode, testCase.returnBody)
 
 			helper.SetConfigValue("aura.beta-enabled", true)
 
