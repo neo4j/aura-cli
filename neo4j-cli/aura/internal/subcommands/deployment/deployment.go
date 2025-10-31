cb69ce70e186dd25edc605fbb2b2e6c293719805
Kim Möller <kim.moller@neo4j.com>
2025-10-31 11:16:04 +0100

    Add get deployment command

---
+++ neo4j-cli/aura/internal/subcommands/deployment/deployment.go
@@ -0,0 +1,49 @@
+package deployment
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/common/clierr"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmd(cfg *clicfg.Config) *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "deployment",
+		Short: "Relates to Fleet Manager deployments",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			cfg.Aura.BindBaseUrl(cmd.Flags().Lookup("base-url"))
+
+			cfg.Aura.BindAuthUrl(cmd.Flags().Lookup("auth-url"))
+
+			outputValue := cmd.Flags().Lookup("output").Value.String()
+			if outputValue != "" {
+				validOutputValue := false
+				for _, v := range clicfg.ValidOutputValues {
+					if v == outputValue {
+						validOutputValue = true
+						break
+					}
+				}
+				if !validOutputValue {
+					return clierr.NewUsageError("invalid output value specified: %s", outputValue)
+				}
+			}
+
+			cfg.Aura.BindOutput(cmd.Flags().Lookup("output"))
+
+			return nil
+		},
+	}
+
+	cmd.AddCommand(NewGetCmd(cfg))
+
+	cmd.PersistentFlags().String("auth-url", "", "")
+	cmd.PersistentFlags().String("base-url", "", "")
+	cmd.PersistentFlags().String("output", "", fmt.Sprintf("Format to print console output in, from a choice of [%s]", strings.Join(clicfg.ValidOutputValues[:], ", ")))
+
+	return cmd
+}
---
+++ neo4j-cli/aura/internal/subcommands/deployment/get.go
@@ -0,0 +1,66 @@
+package deployment
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/api"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewGetCmd(cfg *clicfg.Config) *cobra.Command {
+	var (
+		organizationId string
+		projectId      string
+	)
+
+	const (
+		organizationIdFlag = "organization-id"
+		projectIdFlag      = "project-id"
+	)
+
+	cmd := &cobra.Command{
+		Use:   "get <id>",
+		Short: "Returns deployment details",
+		Long:  "This endpoint returns details about a specific Fleet Manager deployment.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			deploymentId := args[0]
+			path := fmt.Sprintf("/organizations/%s/projects/%s/deployments/%s", organizationId, projectId, deploymentId)
+
+			cmd.SilenceUsage = true
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				Method:  http.MethodGet,
+				Version: api.AuraApiVersion2,
+			})
+			if err != nil {
+				return err
+			}
+
+			spew.Dump(resBody)
+			if statusCode == http.StatusOK {
+				fields := []string{"id", "name", "created_by", "dbms", "token"}
+				output.PrintBody(cmd, cfg, resBody, fields)
+			}
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&organizationId, organizationIdFlag, "", "(required) Organization ID")
+	cmd.Flags().StringVar(&projectId, projectIdFlag, "", "(required) Project/tenant ID")
+
+	err := cmd.MarkFlagRequired(organizationIdFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.MarkFlagRequired(projectIdFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cmd
+}
