0e5a445296c089b232766844628d290123549b50
Kim Möller <kim.moller@neo4j.com>
2025-10-31 11:16:04 +0100

    Add get deployment command

--- neo4j-cli/aura/aura.go
+++ neo4j-cli/aura/aura.go
@@ -1,6 +1,7 @@
 package aura
 
 import (
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/subcommands/deployment"
 	"github.com/neo4j/cli/neo4j-cli/aura/internal/subcommands/graphanalytics"
 	_import "github.com/neo4j/cli/neo4j-cli/aura/internal/subcommands/import"
 	"github.com/spf13/cobra"
@@ -27,6 +28,7 @@ func NewCmd(cfg *clicfg.Config) *cobra.Command {
 	cmd.AddCommand(instance.NewCmd(cfg))
 	cmd.AddCommand(tenant.NewCmd(cfg))
 	cmd.AddCommand(graphanalytics.NewCmd(cfg))
+	cmd.AddCommand(deployment.NewCmd(cfg))
 	if cfg.Aura.AuraBetaEnabled() {
 		cmd.AddCommand(dataapi.NewCmd(cfg))
 		cmd.AddCommand(_import.NewCmd(cfg))
