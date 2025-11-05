bf61195ba191963ac426d55649ad26447e8c5277
Kim Möller <kim.moller@neo4j.com>
2025-11-05 13:52:34 +0100

    Add create and register deployment commands

---
+++ neo4j-cli/aura/internal/subcommands/deployment/create.go
@@ -0,0 +1,79 @@
+package deployment
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/neo4j/cli/common/clicfg"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/api"
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/output"
+	"github.com/spf13/cobra"
+)
+
+func NewCreateCmd(cfg *clicfg.Config) *cobra.Command {
+	var (
+		organizationId string
+		projectId      string
+		name           string
+		connectionUrl  string
+	)
+
+	const (
+		organizationIdFlag = "organization-id"
+		projectIdFlag      = "project-id"
+		nameFlag           = "name"
+		connectionUrlFlag  = "connection-url"
+	)
+
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create a new deployment",
+		Long:  "This endpoint creates a new Fleet Manager deployment.",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path := fmt.Sprintf("/organizations/%s/projects/%s/deployments", organizationId, projectId)
+
+			body := map[string]any{
+				"name":           name,
+				"connection_url": connectionUrl,
+			}
+
+			cmd.SilenceUsage = true
+			resBody, statusCode, err := api.MakeRequest(cfg, path, &api.RequestConfig{
+				Method:   http.MethodPost,
+				PostBody: body,
+				Version:  api.AuraApiVersion2,
+			})
+			if err != nil {
+				return err
+			}
+
+			// NOTE: Deployment create should not return OK (200), it always returns 201, checking both just in case
+			if statusCode == http.StatusCreated || statusCode == http.StatusOK {
+				output.PrintBody(cmd, cfg, resBody, []string{"id"})
+			}
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&organizationId, organizationIdFlag, "", "(required) Organization ID")
+	cmd.Flags().StringVar(&projectId, projectIdFlag, "", "(required) Project/tenant ID")
+	cmd.Flags().StringVar(&name, nameFlag, "", "(required) Deployment name")
+	cmd.Flags().StringVar(&connectionUrl, connectionUrlFlag, "", "An optional connection URL for the deployment")
+
+	err := cmd.MarkFlagRequired(organizationIdFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.MarkFlagRequired(projectIdFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = cmd.MarkFlagRequired(nameFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cmd
+}
