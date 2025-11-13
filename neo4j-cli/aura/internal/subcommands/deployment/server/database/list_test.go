3d0a6084d96859a426d90bf92be8573bf4e7a3bf
Kim Möller <kim.moller@neo4j.com>
2025-11-13 14:37:04 +0100

    Add initial unit tests for deployment commands

---
+++ neo4j-cli/aura/internal/subcommands/deployment/server/database/list_test.go
@@ -0,0 +1,163 @@
+package serverdatabase_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/neo4j/cli/neo4j-cli/aura/internal/test/testutils"
+)
+
+func TestListDeploymentServerDatabase(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	organizationId := "81e4ae5c-171b-4700-b243-8d1dd34f7321"
+	projectId := "ef7faf53-fb7e-4994-8d0f-64ae56e91c42"
+	deploymentId := "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2"
+	serverId := "66c6ee3b-de03-4e8a-ba57-066b34730092"
+
+	mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/organizations/%s/projects/%s/deployments/%s/servers/%s/databases", organizationId, projectId, deploymentId, serverId), http.StatusOK, `{
+		"data": [
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "50BE51F6E095D716850AA6FE7E9E7882677D21D7BB84658A1E171A55F24B143C",
+				"last_committed_txn": 2,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "neo4j",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "standard",
+				"writer": true
+			},
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "7D80871B447965646A0AE2AE844719308EFE00CBB0F9195FB0F3E12FA3E7D018",
+				"last_committed_txn": 139,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "system",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "system",
+				"writer": true
+			}
+		]
+	}`)
+
+	helper.SetConfigValue("aura.beta-enabled", true)
+	helper.SetConfigValue("aura.output", "json")
+	helper.ExecuteCommand(fmt.Sprintf("deployment server database list --deployment-id=%s --server-id=%s --organization-id=%s --project-id=%s", deploymentId, serverId, organizationId, projectId))
+
+	mockHandler.AssertCalledTimes(1)
+	mockHandler.AssertCalledWithMethod(http.MethodGet)
+
+	helper.AssertOutJson(`{
+		"data": [
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "50BE51F6E095D716850AA6FE7E9E7882677D21D7BB84658A1E171A55F24B143C",
+				"last_committed_txn": 2,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "neo4j",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "standard",
+				"writer": true
+			},
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "7D80871B447965646A0AE2AE844719308EFE00CBB0F9195FB0F3E12FA3E7D018",
+				"last_committed_txn": 139,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "system",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "system",
+				"writer": true
+			}
+		]
+	}`)
+}
+
+func TestListDeploymentServerDatabaseWithTableOutput(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	organizationId := "81e4ae5c-171b-4700-b243-8d1dd34f7321"
+	projectId := "ef7faf53-fb7e-4994-8d0f-64ae56e91c42"
+	deploymentId := "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2"
+	serverId := "66c6ee3b-de03-4e8a-ba57-066b34730092"
+
+	mockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v2beta1/organizations/%s/projects/%s/deployments/%s/servers/%s/databases", organizationId, projectId, deploymentId, serverId), http.StatusOK, `{
+		"data": [
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "50BE51F6E095D716850AA6FE7E9E7882677D21D7BB84658A1E171A55F24B143C",
+				"last_committed_txn": 2,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "neo4j",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "standard",
+				"writer": true
+			},
+			{
+				"current_status": "online",
+				"deployment_id": "9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2",
+				"graph_shards": null,
+				"id": "7D80871B447965646A0AE2AE844719308EFE00CBB0F9195FB0F3E12FA3E7D018",
+				"last_committed_txn": 139,
+				"last_seen": "2025-11-11T13:56:54.487554Z",
+				"name": "system",
+				"property_shards": null,
+				"replication_lag": 0,
+				"role": "primary",
+				"server_id": "66c6ee3b-de03-4e8a-ba57-066b34730092",
+				"status_message": "",
+				"type": "system",
+				"writer": true
+			}
+		]
+	}`)
+
+	helper.SetConfigValue("aura.beta-enabled", true)
+	helper.SetConfigValue("aura.output", "table")
+	helper.ExecuteCommand(fmt.Sprintf("deployment server database list --deployment-id=%s --server-id=%s --organization-id=%s --project-id=%s", deploymentId, serverId, organizationId, projectId))
+
+	mockHandler.AssertCalledTimes(1)
+	mockHandler.AssertCalledWithMethod(http.MethodGet)
+
+	helper.AssertOut(`
+┌──────────────────────────────────────────────────────────────────┬──────────────────────────────────────┬──────────────────────────────────────┬────────┬──────────┬────────────────┬────────────────┬──────────────┬────────────────────┬─────────────────────────────┬─────────────────┬─────────────────┬─────────┬────────┐
+│ ID                                                               │ DEPLOYMENT_ID                        │ SERVER_ID                            │ NAME   │ TYPE     │ CURRENT_STATUS │ STATUS_MESSAGE │ GRAPH_SHARDS │ LAST_COMMITTED_TXN │ LAST_SEEN                   │ PROPERTY_SHARDS │ REPLICATION_LAG │ ROLE    │ WRITER │
+├──────────────────────────────────────────────────────────────────┼──────────────────────────────────────┼──────────────────────────────────────┼────────┼──────────┼────────────────┼────────────────┼──────────────┼────────────────────┼─────────────────────────────┼─────────────────┼─────────────────┼─────────┼────────┤
+│ 50BE51F6E095D716850AA6FE7E9E7882677D21D7BB84658A1E171A55F24B143C │ 9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2 │ 66c6ee3b-de03-4e8a-ba57-066b34730092 │ neo4j  │ standard │ online         │                │              │ 2                  │ 2025-11-11T13:56:54.487554Z │                 │ 0               │ primary │ true   │
+│ 7D80871B447965646A0AE2AE844719308EFE00CBB0F9195FB0F3E12FA3E7D018 │ 9a1e6181-7d0b-48a2-bc2b-4250c36b5cc2 │ 66c6ee3b-de03-4e8a-ba57-066b34730092 │ system │ system   │ online         │                │              │ 139                │ 2025-11-11T13:56:54.487554Z │                 │ 0               │ primary │ true   │
+└──────────────────────────────────────────────────────────────────┴──────────────────────────────────────┴──────────────────────────────────────┴────────┴──────────┴────────────────┴────────────────┴──────────────┴────────────────────┴─────────────────────────────┴─────────────────┴─────────────────┴─────────┴────────┘
+	`)
+}
