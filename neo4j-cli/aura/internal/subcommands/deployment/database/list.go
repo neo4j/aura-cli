b74b360f1e3ad0618e5c8bdf85dc1c70e08bbd2c
Kim Möller <kim.moller@neo4j.com>
2025-11-20 16:03:32 +0100

    Add shorthands for deployment commands

--- neo4j-cli/aura/internal/subcommands/deployment/database/list.go
+++ neo4j-cli/aura/internal/subcommands/deployment/database/list.go
@@ -68,9 +68,9 @@ func NewListCmd(cfg *clicfg.Config) *cobra.Command {
 			return nil
 		},
 	}
-	cmd.Flags().StringVar(&organizationId, organizationIdFlag, "", "(required) Organization ID")
-	cmd.Flags().StringVar(&projectId, projectIdFlag, "", "(required) Project/tenant ID")
-	cmd.Flags().StringVar(&deploymentId, deploymentIdFlag, "", "(required) Deployment ID")
+	cmd.Flags().StringVarP(&organizationId, organizationIdFlag, "o", "", "(required) Organization ID")
+	cmd.Flags().StringVarP(&projectId, projectIdFlag, "p", "", "(required) Project/tenant ID")
+	cmd.Flags().StringVarP(&deploymentId, deploymentIdFlag, "d", "", "(required) Deployment ID")
 
 	err := cmd.MarkFlagRequired(organizationIdFlag)
 	if err != nil {
