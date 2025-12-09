acbf90573f76b8b63a06d15fcc60dae4b6732b7c
Kim Möller <kim.moller@neo4j.com>
2025-12-09 15:12:43 +0100

    Move deployment command behind beta flag

--- neo4j-cli/aura/aura.go
+++ neo4j-cli/aura/aura.go
@@ -28,10 +28,10 @@ func NewCmd(cfg *clicfg.Config) *cobra.Command {
 	cmd.AddCommand(instance.NewCmd(cfg))
 	cmd.AddCommand(tenant.NewCmd(cfg))
 	cmd.AddCommand(graphanalytics.NewCmd(cfg))
-	cmd.AddCommand(deployment.NewCmd(cfg))
 	if cfg.Aura.AuraBetaEnabled() {
 		cmd.AddCommand(dataapi.NewCmd(cfg))
 		cmd.AddCommand(_import.NewCmd(cfg))
+		cmd.AddCommand(deployment.NewCmd(cfg))
 	}
 
 	return cmd
