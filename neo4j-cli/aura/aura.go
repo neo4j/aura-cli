71a4955d18944429ccf352e15afa41343467597b
Kim Möller <kim.moller@neo4j.com>
2026-01-16 16:35:57 +0100

    Move setting under v2 beta

--- neo4j-cli/aura/aura.go
+++ neo4j-cli/aura/aura.go
@@ -25,12 +25,12 @@ func NewCmd(cfg *clicfg.Config) *cobra.Command {
 
 	cmd.AddCommand(config.NewCmd(cfg))
 	cmd.AddCommand(credential.NewCmd(cfg))
-	cmd.AddCommand(setting.NewCmd(cfg))
 	cmd.AddCommand(customermanagedkey.NewCmd(cfg))
 	cmd.AddCommand(instance.NewCmd(cfg))
 	cmd.AddCommand(tenant.NewCmd(cfg))
 	cmd.AddCommand(graphanalytics.NewCmd(cfg))
 	if cfg.Aura.AuraBetaEnabled() {
+		cmd.AddCommand(setting.NewCmd(cfg))
 		cmd.AddCommand(dataapi.NewCmd(cfg))
 		cmd.AddCommand(_import.NewCmd(cfg))
 		cmd.AddCommand(deployment.NewCmd(cfg))
