91739994b89bba2f53e6a577a59a1db9d96eace0
Vivien Tran <vivientran@Viviens-MacBook-Pro.local>
2026-05-11 11:46:37 +0100

    remove whitespaces

--- neo4j-cli/aura/internal/subcommands/tenant/get.go
+++ neo4j-cli/aura/internal/subcommands/tenant/get.go
@@ -24,7 +24,6 @@ func NewGetCmd(cfg *clicfg.Config) *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			tenantId := strings.TrimSpace(args[0])
-
 			path := fmt.Sprintf("/tenants/%s", tenantId)
 
 			cmd.SilenceUsage = true
