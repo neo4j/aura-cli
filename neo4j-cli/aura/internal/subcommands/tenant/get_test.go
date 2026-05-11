776054e386a774f76c6241927b72940ec330eec4
Vivien Tran <vivien.tran@neo4j.com>
2026-05-11 11:50:57 +0100

    add test

--- neo4j-cli/aura/internal/subcommands/tenant/get_test.go
+++ neo4j-cli/aura/internal/subcommands/tenant/get_test.go
@@ -112,6 +112,46 @@ func TestGetTenantNotFoundError(t *testing.T) {
 	helper.AssertErr("Error: [The tenant you specified could not be found]")
 }
 
+func TestGetTenantWithTrailingNewlineInId(t *testing.T) {
+	helper := testutils.NewAuraTestHelper(t)
+	defer helper.Close()
+
+	tenantId := "6981ace7-efe8-4f5c-b7c5-267b5162ce91"
+
+	getMockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v1/tenants/%s", tenantId), http.StatusOK, `{
+			"data": {
+				"id": "6981ace7-efe8-4f5c-b7c5-267b5162ce91",
+				"name": "Production",
+				"instance_configurations": []
+			}
+		}`)
+
+	metricsIntegrationMockHandler := helper.NewRequestHandlerMock(fmt.Sprintf("/v1/tenants/%s/metrics-integration", tenantId), http.StatusBadRequest, `{
+			"errors": [
+				{
+					"message": "This tenant has no instances eligible for metrics integration",
+					"reason": "tenant-incapable-of-action"
+				}
+			]
+		}`)
+
+	helper.ExecuteCommand(fmt.Sprintf("tenant get \"%s\n\"", tenantId))
+
+	getMockHandler.AssertCalledTimes(1)
+	getMockHandler.AssertCalledWithMethod(http.MethodGet)
+	metricsIntegrationMockHandler.AssertCalledTimes(1)
+	metricsIntegrationMockHandler.AssertCalledWithMethod(http.MethodGet)
+
+	helper.AssertOutJson(`{
+		"data": {
+			"id": "6981ace7-efe8-4f5c-b7c5-267b5162ce91",
+			"instance_configurations": [],
+			"name": "Production"
+		}
+	}
+	`)
+}
+
 func TestGetTenantWithTableOutput(t *testing.T) {
 	helper := testutils.NewAuraTestHelper(t)
 	defer helper.Close()
